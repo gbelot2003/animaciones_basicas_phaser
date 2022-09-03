1a1510d1c640a50bc0014149a3b740cf738f67eb
Gerardo Belot <gbelot2003@hotmail.com>
2022-09-03 17:25:41 -0600

    Primer commit

---
+++ src/init.js
@@ -0,0 +1,33 @@
+const config = {
+    width: 480,
+    height: 320,
+    parent: 'container',
+    pixelArt: true,
+    scene: {
+        preload,
+        create
+    }
+}
+
+new Phaser.Game(config)
+
+function preload() {
+    this.load.spritesheet('evil_tomato', './assets/evil_tomato.png', {
+        frameWidth: 16,
+        frameHeight: 25
+    })
+}
+
+function create() {
+    this.tomato = this.add.sprite(100, 100, 'evil_tomato', 1);
+    this.anims.create({
+        key: 'tomato_walk',
+        frames: this.anims.generateFrameNumbers('evil_tomato', {
+            frames: [0, 1, 2, 3, 4, 5, 6, 7, 8]
+        }),
+        repeat: -1,
+        frameRate: 10
+    });
+
+this.tomato.anims.play('tomato_walk')
+}
